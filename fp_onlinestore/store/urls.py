d92120367961f1f5d7422afe23cabb97394da6c9
Sergei <vitjaz89@list.ru>
2021-04-11 11:01:51 +0300

    adding
    smart/note create/del works

--- fp_onlinestore/store/urls.py
+++ fp_onlinestore/store/urls.py
@@ -1,14 +1,18 @@
 
 from django.urls import path
 from .views import test_view, NotebookDetailView, NotebookListView, SmartphoneListView, SmartphoneDetailView
+from .views import SmartphoneCreateView, NotebookCreateView, NotebookDeleteView, SmartphoneDeleteView
 
 urlpatterns = [
     path('', test_view, name='base'),
     path('notebooks/<int:pk>', NotebookDetailView.as_view(), name='notebook_detail'),
     path('notebooks', NotebookListView.as_view(), name='notebook_list'),
     path('smartphones', SmartphoneListView.as_view(), name='smartphone_list'),
-    path('smartphones/<int:pk>', SmartphoneDetailView.as_view(), name='smartphone_detail')
-
+    path('smartphones/<int:pk>', SmartphoneDetailView.as_view(), name='smartphone_detail'),
+    path('notebooks/create/', NotebookCreateView.as_view(), name='notebook_create'),
+    path('smartphones/create/', SmartphoneCreateView.as_view(), name='smartphone_create'),
+    path('notebooks/<int:pk>/delete', NotebookDeleteView.as_view(), name='notebook_delete'),
+    path('smartphones/<int:pk>/delete', SmartphoneDeleteView.as_view(), name='smartphone_delete')
 
 
 ]
--- fp_onlinestore/store/views.py
+++ fp_onlinestore/store/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView, ListView
 from .models import Notebook, Smartphone
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
 # Create your views here.
 
 
@@ -41,3 +47,31 @@ class SmartphoneListView(ListView):
         smartphones = Smartphone.objects.all()
 
         return smartphones
+
+class NotebookCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = ['store.add_notebook']
+    template_name = 'notebook_create.html'
+    model = Notebook
+    fields = '__all__'
+    success_url = reverse_lazy('notebooks_list')
+
+class SmartphoneCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = ['store.add_smartphone']
+    template_name = 'smartphone_create.html'
+    model = Smartphone
+    fields = '__all__'
+    success_url = reverse_lazy('smartphones_list')
+
+class NotebookDeleteView(PermissionRequiredMixin, DeleteView):
+    permission_required = ['store.delete_notebook']
+    template_name = 'notebook_delete.html'
+    model = Notebook
+    context_object_name = 'notebook'
+    success_url = reverse_lazy('notebook_list')
+
+class SmartphoneDeleteView(PermissionRequiredMixin, DeleteView):
+    permission_required = ['store.delete_smartphone']
+    template_name = 'smartphone_delete.html'
+    model = Smartphone
+    context_object_name = 'smartphone'
+    success_url = reverse_lazy('smartphone_list')
